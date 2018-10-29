7e8a8fc8e9bc4d699f267cd5f9d3241ca066ff29
DOGOD <return3244@gmail.com>
2018-10-29 10:15:21 +0800

    fixed

--- Julia/DeepLearning/Activation.jl
+++ Julia/DeepLearning/Activation.jl
@@ -6,7 +6,7 @@
         σ(x) = --------
                 1 + eˣ
     """
-    function sigmoid!(x::Array)
+    function sigmoid(x::Array)
         return 1 ./ 1 .+ exp.(-x); end
 
     """
@@ -16,7 +16,7 @@
         ReLu(x) = max(0, x)
 
     """
-    function ReLU!(x::Array)
+    function ReLU(x::Array)
         killer = (x .<= 0);
         x[killer] .= 0;
     return x end
@@ -29,12 +29,12 @@
         tanh = --------
                eˣ + e⁻ˣ
     """
-    function tanh!(x::Array)
+    function tanh(x::Array)
         return tanh.(x) end
 
 function _Activation_(_main_=false)
     if !_main_
-        println('\n', "Import function:")
+        println('\n',"Import function:")
         println("----------")
         println(_List)
     else
@@ -46,4 +46,4 @@ function _Activation_(_main_=false)
         print( tanh.(X) );end
 end
 
-_List = ["sigmoid!" "ReLU!" "tanh!"];
+_List = ["sigmoid" "ReLU" "tanh"];
--- Julia/DeepLearning/MeanSquaredError.jl
+++ Julia/DeepLearning/MeanSquaredError.jl
@@ -13,26 +13,6 @@ using LinearAlgebra
         return dif
     end
 
-# Gradicent Descent
-function GD(y_true::AbstractVector{<:Number}, y_pred::AbstractVector{<:Number}, lr=.001)
-    println(y_true)
-    dim = length(y_true);
-    grad = float.(zeros(dim));
-    for idx in 1:dim
-        δᵥ = zeros(dim);
-        δᵥ[idx] = 1;
-        δᵥ[idx] = MSE(y_true, y_pred+δᵥ);
-        δᵥ = δᵥ ./ dot(δᵥ, δᵥ);
-        grad += δᵥ;
-    end
-    y_pred -= lr*grad;
-    return y_pred
-end
 
 y_pred = float.( [ x^2 for x in 1:5 ] );
 y_true = zeros(5);
-
-ans = MSE(y_true, y_pred)
-
-ans = GD(y_pred, y_true)
-println(ans)
