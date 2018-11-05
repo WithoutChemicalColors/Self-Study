0cbaf14f5956bdda5cfec510e5cccfe6d1bdaf7d
DOGOD <return3244@gmail.com>
2018-11-06 00:37:25 +0800

    a021

---
+++ Cpp/HS/a021.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+void convert(string sent){
+    
+}
+
+int main(int argc, char **argv){
+    string left, right;
+    char op;
+    while(cin>> left >> op >> right ){
+        cout<< left << " " << op << " " << right << endl;
+    }
+    return 0;
+}
